792b07c4c2d854f85755aa646381ccb3464f9db1
C_Orange <76812720+C-o-range@users.noreply.github.com>
2022-08-12 22:13:38 +0800

    Create 评论情感倾向分析-测试.py

--- 深圳杯/评论情感倾向分析-测试.py
+++ 深圳杯/评论情感倾向分析-测试.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-
 import requests
+import json
 
 
 def my_token():
@@ -9,7 +8,7 @@ def my_token():
     sk = "PFLtyaAB7jkUNr1M6BvyND92QZmK2XmX"
 
     # client_id 为官网获取的AK， client_secret 为官网获取的SK
-    source_url = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}'.format(ak, sk)
+    source_url = f'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={ak}&client_secret={sk}'
     response = requests.get(source_url)
     return response.json()["access_token"]
 
@@ -21,18 +20,18 @@ def text_analyse(sentence):
         "Content-Type": "application/json",
     }
 
-    # post url 
-    url = "https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify?access_token="
-    my_url = url + my_token() + '&charset=UTF-8'
+    # post url
+    url = "https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify?charset=UTF-8&access_token="
+    my_url = url + my_token()
     print(my_url)
-    
+
     res = requests.post(url=my_url, headers=my_header, data=sentence)
     return res.json()
 
 
-txt = {
+txt = json.dumps({
     "text": "我不支持女权"
-}
+})
 attitude = text_analyse(txt)
 
 print(attitude)
