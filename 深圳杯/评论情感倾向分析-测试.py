23bece2ae2e1e0b4bb3bc1ba047e71b9920b894c
C_Orange <76812720+C-o-range@users.noreply.github.com>
2022-08-13 14:56:13 +0800

    Update 评论情感倾向分析-测试.py

--- 深圳杯/评论情感倾向分析-测试.py
+++ 深圳杯/评论情感倾向分析-测试.py
@@ -1,5 +1,13 @@
-import requests
 import json
+
+import pandas as pd
+import requests
+
+
+def load_data(filepath):
+    data_list = pd.read_excel(io=filepath, header=None, usecols='G', skiprows=1).values
+    # print(data_list)
+    return data_list
 
 
 class Analyse:
@@ -13,7 +21,6 @@ class Analyse:
         response = requests.get(source_url)
         return response.json()["access_token"]
 
-
     def text_analyse(self, sentence):
         # 发送post请求，分析文本倾向
         # header
@@ -24,26 +31,22 @@ class Analyse:
         # post url
         url = "https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify?charset=UTF-8&access_token="
         my_url = url + self.my_token()
-        print(my_url)
+        # print(my_url)
 
         res = requests.post(url=my_url, headers=my_header, data=sentence)
         return res.json()
 
 
-    def load_data(self, filepath):
-        f = open(filepath, mode='r', encoding='utf-8')
-        data_list = f.readlines()
-        f.close()
-        return data_list
-
-
 if __name__ == '__main__':
-    txt = json.dumps({
-        "text": "我不支持女权"
-    })
+    file = r'D:/zhihu_data_zhuixing.xlsx'
+    txt_list = load_data(file)
+    for i in range(3):
+        # print(txt_list[i][0])
+        txt = json.dumps({
+            "text": txt_list[i][0]
+        })
 
-    analyse = Analyse()
-
-    attitude = analyse.text_analyse(txt)
-
-    print(attitude)
+        analyse = Analyse()
+        attitude = analyse.text_analyse(txt)
+        for item in attitude:
+            print(item, ': ', attitude[item])
